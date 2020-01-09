9518e6253a492dfef5e94aa69a43a0733a721764
vikas <vikas.on@gmail.com>
2020-01-10 00:57:07 +0700

    initial version

---
+++ src/main/kotlin/org/personal/finance/PersonalFinance/PersonalFinanceApplication.kt
@@ -0,0 +1,11 @@
+package org.personal.finance.PersonalFinance
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PersonalFinanceApplication
+
+fun main(args: Array<String>) {
+	runApplication<PersonalFinanceApplication>(*args)
+}
---
+++ src/test/kotlin/org/personal/finance/PersonalFinance/PersonalFinanceApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.personal.finance.PersonalFinance
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PersonalFinanceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
