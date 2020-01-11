baf84ae7ab4161d648964a8c4dd33cd8834cbda5
vikas <vikas.on@gmail.com>
2020-01-11 23:21:17 +0700

    fixing swagger issue

--- src/main/kotlin/org/personal/finance/PersonalFinance/config/SwaggerConfig.kt
+++ src/main/kotlin/org/personal/finance/PersonalFinance/config/SwaggerConfig.kt
@@ -24,3 +24,17 @@ class SwaggerConfig {
                     .build()
 
 }
+
+
+@Configuration
+class WebFLuxConfig : WebFluxConfigurer {
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry.addResourceHandler("/swagger-ui.html**")
+                .addResourceLocations("classpath:/META-INF/resources/")
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/")
+        super.addResourceHandlers(registry);
+
+    }
+}
+
